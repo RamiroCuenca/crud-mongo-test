eb52166ca166bd12eae70ad950dffe1c6f4c96da
Ramiro Cuenca Salinas <ramicuenca00@gmail.com>
2021-11-13 13:48:52 -0300

    Created Common package

---
+++ common/response.go
@@ -0,0 +1,17 @@
+package common
+
+import "net/http"
+
+// Send response to a successful http request
+func SendResponse(w http.ResponseWriter, status int, data []byte) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(status)
+	w.Write(data)
+}
+
+// Send response to an unseccessful htpp request
+func SendError(w http.ResponseWriter, status int, data []byte) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(status)
+	w.Write(data)
+}
