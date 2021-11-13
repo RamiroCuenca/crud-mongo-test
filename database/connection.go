769ad91f3f275845a40a9d58ca01680d6b063275
Ramiro Cuenca Salinas <ramicuenca00@gmail.com>
2021-11-13 18:37:37 -0300

    Created connection to MongoDatabase

---
+++ database/connection.go
@@ -0,0 +1,46 @@
+package database
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"time"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/mongo/readpref"
+	"gopkg.in/mgo.v2/bson"
+)
+
+func GetMongoClient() *mongo.Client {
+	// database is the service from docker-compose
+	uri := "mongodb://admin:password@localhost:27018" // uniform resource identifier
+
+	// Connect to the database
+	client, err := mongo.NewClient(options.Client().ApplyURI(uri))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+
+	err = client.Connect(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Check if the connection is stable
+	err = client.Ping(ctx, readpref.Primary())
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// We can fetch all available databases from the connection
+	databases, err := client.ListDatabaseNames(ctx, bson.M{})
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(databases)
+
+	return client
+}
