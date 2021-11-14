39afbb91153ac0b9f0af8706853d19671bca87bc
Ramiro Cuenca Salinas <ramicuenca00@gmail.com>
2021-11-14 11:47:48 -0300

    Created a function that generates and other that validates JWT tokens

---
+++ auth/token.go
@@ -0,0 +1,63 @@
+package auth
+
+import (
+	"errors"
+	"time"
+
+	"github.com/RamiroCuenca/vozy-test/users/models"
+	"github.com/golang-jwt/jwt/v4"
+)
+
+// Generates a JWT. It receives the data from the user who has logged in
+func GenerateToken(data models.User) (string, error) {
+	claim := models.Claim{
+		Username: data.Username,
+		StandardClaims: jwt.StandardClaims{
+			ExpiresAt: time.Now().Add(time.Hour * 2).Unix(),
+			Issuer:    "Ramiro Cuenca",
+		},
+	}
+
+	// Prepared the token to be signed with RS256 method and including the claim
+	token := jwt.NewWithClaims(jwt.SigningMethodRS256, claim)
+
+	// Sign the token with our private key
+	signedToken, err := token.SignedString(signKey)
+	if err != nil {
+		return "", err
+	}
+
+	// Return the generated token
+	return signedToken, nil
+}
+
+// Validate the JWT.
+// Returns the claim so that we can access the information of the user.
+//
+// Before, we need to create a function wich return us the information of
+// our public key (Already parsed, it is declared as "verifyKey")
+func ValidateToken(t string) (models.Claim, error) {
+
+	token, err := jwt.ParseWithClaims(t, &models.Claim{}, verifyFunction)
+	if err != nil {
+		return models.Claim{}, err
+	}
+
+	// Check if the token is valid
+	if !token.Valid {
+		return models.Claim{}, errors.New("Invalid token")
+	}
+
+	// Obtain the claims from the token
+	claim, ok := token.Claims.(*models.Claim)
+	if !ok {
+		return models.Claim{}, errors.New("Couldn't fetch the claims")
+	}
+
+	return *claim, nil
+}
+
+// Returns the verifyKey wich is out public key already parsed
+func verifyFunction(t *jwt.Token) (interface{}, error) {
+	return verifyKey, nil
+}
