9bbafabdd47a1dac28e930613a57f319561ef244
Ramiro Cuenca Salinas <ramicuenca00@gmail.com>
2021-11-14 11:41:55 -0300

    Created a function that loads the keys certificates

---
+++ auth/certificates.go
@@ -0,0 +1,60 @@
+package auth
+
+import (
+	"crypto/rsa"
+	"io/ioutil"
+	"sync"
+
+	"github.com/golang-jwt/jwt/v4"
+)
+
+var (
+	signKey   *rsa.PrivateKey
+	verifyKey *rsa.PublicKey
+	once      sync.Once
+)
+
+// We left them private (Only can be used by this package)
+// so that the other packages can not manipulate them
+
+// Singleton that assign a value to this vars only once (EXPORTED)
+func LoadCertificates(privateFile, publicFile string) error {
+	var err error
+
+	once.Do(func() {
+		err = loadCertificates(privateFile, publicFile)
+	})
+
+	return err
+}
+
+// Loads the certicates and send them to be parsed
+func loadCertificates(privateFile, publicFile string) error {
+	privateBytes, err := ioutil.ReadFile(privateFile)
+	if err != nil {
+		return err
+	}
+
+	publicBytes, err := ioutil.ReadFile(publicFile)
+	if err != nil {
+		return err
+	}
+
+	return parseRSA(privateBytes, publicBytes)
+}
+
+// Parces the certicates
+func parseRSA(privateBytes, publicBytes []byte) error {
+	var err error
+	signKey, err = jwt.ParseRSAPrivateKeyFromPEM(privateBytes)
+	if err != nil {
+		return err
+	}
+
+	verifyKey, err = jwt.ParseRSAPublicKeyFromPEM(publicBytes)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
