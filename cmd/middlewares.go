e83c7f3a18b8ebaab18f835278a9cb024daddf27
Ramiro Cuenca Salinas <ramicuenca00@gmail.com>
2021-11-14 12:22:05 -0300

    Created authentication middleware

---
+++ cmd/middlewares.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/RamiroCuenca/crud-mongo-test/auth"
+	"github.com/RamiroCuenca/crud-mongo-test/common"
+)
+
+// Verifys that the user sent a valid JWT token
+func AuthenticationMiddleware(f func(w http.ResponseWriter, r *http.Request)) func(w http.ResponseWriter, r *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		token := r.Header.Get("Authorization")
+		_, err := auth.ValidateToken(token) // auth is the package we created
+		// If token is invalid
+		if err != nil {
+			forbidden(w, r)
+			return
+		}
+
+		f(w, r)
+	}
+}
+
+// If the user is not authenticated it deprecates the request and returns a forbidden status
+func forbidden(w http.ResponseWriter, r *http.Request) {
+	json := []byte(`{
+		"message": "It hasn't got authorization"
+	}`)
+	common.SendError(w, http.StatusForbidden, json)
+}
