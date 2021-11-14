f4ac880b0eb2ad32821c7f9370ed6c6cc22c4c0d
Ramiro Cuenca Salinas <ramicuenca00@gmail.com>
2021-11-14 12:28:23 -0300

    Loaded certificates from main package

--- cmd/main.go
+++ cmd/main.go
@@ -1,15 +1,23 @@
 package main
 
 import (
+	"log"
 	"net/http"
 	"sync"
 
+	"github.com/RamiroCuenca/crud-mongo-test/auth"
 	"github.com/RamiroCuenca/crud-mongo-test/routes"
 )
 
 var once sync.Once
 
 func main() {
+	// Parse the certificates/keys (JWT)
+	err := auth.LoadCertificates("certificates/app.rsa", "certificates/app.rsa.pub")
+	if err != nil {
+		log.Fatalf("Could not load the certificates/keys. Error: %v", err)
+	}
+
 	// Get router
 	mux := routes.GetRouter()
 
