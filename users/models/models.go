1949f2ca2af1ae57c4c98a2a9aea2339e5d52e4e
Ramiro Cuenca Salinas <ramicuenca00@gmail.com>
2021-11-14 11:35:06 -0300

    Created a model for JWT Claims

--- users/models/models.go
+++ users/models/models.go
@@ -1,9 +1,19 @@
 package models
 
-import "go.mongodb.org/mongo-driver/bson/primitive"
+import (
+	"github.com/golang-jwt/jwt/v4"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
 
+// Model for User object
 type User struct {
 	Id       primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
 	Username string             `json:"username,omitempty" bson:"username,omitempty"`
 	Password string             `json:"password,omitempty" bson:"password,omitempty"`
+}
+
+// Claim is the information that will be sent through the JWT Payload
+type Claim struct {
+	Username string `json:"username"`
+	jwt.StandardClaims
 }
