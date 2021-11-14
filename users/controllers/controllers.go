8275d14c7871ad11d6cb712e60309101be49a118
Ramiro Cuenca Salinas <ramicuenca00@gmail.com>
2021-11-14 09:40:32 -0300

    Created a controller to log in as user

--- users/controllers/controllers.go
+++ users/controllers/controllers.go
@@ -11,29 +11,21 @@ import (
 	"github.com/RamiroCuenca/vozy-test/database"
 	"github.com/RamiroCuenca/vozy-test/users/models"
 	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+	"gopkg.in/mgo.v2/bson"
 )
 
-// SignUp or register a new user
-func Create(w http.ResponseWriter, r *http.Request) {
+// Register a new user
+func SignUp(w http.ResponseWriter, r *http.Request) {
 	// Create a object where we are going to store the user values
-	var user models.User
-
-	// Decode the username and password from request body
-	err := json.NewDecoder(r.Body).Decode(&user)
+	user, err := decodeDataFromBody(w, r)
 	if err != nil {
-		data := fmt.Sprintf(`{
-			"message": "Couldn't decode the username or password from request body",
-			"error": %s
-		}`, err.Error())
-		common.SendError(w, http.StatusBadRequest, []byte(data))
 		return
 	}
 
-	// Fetch the database
-	mongoClient := database.GetMongoClient()
-	usersCollection := mongoClient.Database("database").Collection("users")
-
-	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+	// Fetch users collection
+	var usersCollection *mongo.Collection
+	usersCollection, ctx := fetchConnection()
 
 	// Insert the user on the Collection
 	result, err := usersCollection.InsertOne(ctx, user)
@@ -50,12 +42,6 @@ func Create(w http.ResponseWriter, r *http.Request) {
 
 	user.Id = result.InsertedID.(primitive.ObjectID)
 
-	// if oid, ok := result.InsertedID.(primitive.ObjectID); ok {
-	// 	user.Id = oid
-	// }
-
-	// user.Id = result.InsertedID
-
 	// Return the created user as a json
 	json, err := json.Marshal(user)
 
@@ -66,4 +52,76 @@ func Create(w http.ResponseWriter, r *http.Request) {
 
 	// Send response
 	common.SendResponse(w, http.StatusOK, []byte(data))
+}
+
+// Log in with an existing user
+func SignIn(w http.ResponseWriter, r *http.Request) {
+	// Create a object where we are going to store the user values
+	user, err := decodeDataFromBody(w, r)
+	if err != nil {
+		return
+	}
+
+	// Check Username and password are not empty
+	if user.Username == "" || user.Password == "" {
+		data := `{
+			"message": "Username nor password can be empty. Try sending valid values"
+		}`
+		common.SendError(w, http.StatusBadRequest, []byte(data))
+		return
+	}
+
+	// Fetch users collection
+	var usersCollection *mongo.Collection
+	usersCollection, ctx := fetchConnection()
+
+	// Search for the user
+	err = usersCollection.FindOne(ctx, bson.M{"username": user.Username, "password": user.Password}).Decode(&user)
+	if err != nil {
+		data := `{
+			"message": "We couldnt fetch any user with provided credentials"
+		}`
+		common.SendError(w, http.StatusBadRequest, []byte(data))
+		return
+	}
+
+	// Return the fetched user as a json
+	json, err := json.Marshal(user)
+
+	data := fmt.Sprintf(`{
+		"message": "User logged in successfully",
+		"user": %s
+	}`, json)
+
+	// Send response
+	common.SendResponse(w, http.StatusOK, []byte(data))
+}
+
+// Decodes username and password from request body and returns a User object
+func decodeDataFromBody(w http.ResponseWriter, r *http.Request) (models.User, error) {
+	var u models.User
+
+	// Decode the username and password from request body
+	err := json.NewDecoder(r.Body).Decode(&u)
+	if err != nil {
+		data := fmt.Sprintf(`{
+		"message": "Couldn't decode the username or password from request body",
+		"error": %s
+	}`, err.Error())
+		common.SendError(w, http.StatusBadRequest, []byte(data))
+		return u, err
+	}
+
+	return u, nil
+}
+
+// Fetch users collection and the context
+func fetchConnection() (*mongo.Collection, context.Context) {
+	// Fetch the database
+	mongoClient := database.GetMongoClient()
+	usersCollection := mongoClient.Database("database").Collection("users")
+
+	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+
+	return usersCollection, ctx
 }
